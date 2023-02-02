ee3a0e94a1a2033b25ad3e44234f720c0b3a2dfb
belovetech <belovetech@gmail.com>
2023-02-02 15:01:01 +0100

    [Fix]: redacting formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,6 +29,7 @@ def filter_datum(fields: List[str], redaction: str,
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
