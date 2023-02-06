16cea29682a1988293b3fa6d9f091332038b5cbb
belovetech <belovetech@gmail.com>
2023-02-06 18:47:03 +0100

    [Update]: document before_request method

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
+"""Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
-auth_type = getenv('AUTH_TYPE')
+auth_type = getenv('AUTH_TYPE', 'auth')
 
 if auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.before_request
 def before_request():
+    """Authenticate users before preocess the request
+    """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
     if auth is None:
