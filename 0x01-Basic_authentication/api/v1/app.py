5e9f8e0c420ffbc662566f5da0c9f6aef8743f0b
belovetech <belovetech@gmail.com>
2023-02-06 18:49:58 +0100

    [Update]: authenticate based on authentication type chosen

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,12 +13,13 @@ from api.v1.auth.basic_auth import BasicAuth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-else:
+if auth_type == 'auth':
     auth = Auth()
 
 
