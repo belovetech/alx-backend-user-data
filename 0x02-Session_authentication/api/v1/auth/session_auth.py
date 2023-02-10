7639c29f6fa65c9f2c3ea11918590d923c1a1c03
belovetech <belovetech@gmail.com>
2023-02-11 00:46:46 +0100

    [Update]: create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 """Representation of session authentication class
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Session auth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session id for user
+        """
+        if user_id and isinstance(user_id, str):
+            session_id = uuid.uuid4()
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
