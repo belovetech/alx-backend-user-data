2cc91f16e086edddf66ddb90ac3fefc120f8f113
belovetech <belovetech@gmail.com>
2023-02-11 01:55:47 +0100

    [Update]: check for authentication before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,16 +31,20 @@ def before_request():
     """Authenticate users before preocess the request
     """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
     if auth.require_auth(request.path, excluded_paths) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
-    request.current_user = auth.current_user(request)
+
+    request.current_user = current_user
 
 
 @app.errorhandler(404)
