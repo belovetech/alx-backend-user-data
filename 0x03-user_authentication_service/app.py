86b145d0cf4a37756e434ad3dcf4918c47942d9f
belovetech <belovetech@gmail.com>
2023-02-14 23:50:17 +0100

    [Feat]: update password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -104,7 +104,7 @@ def get_reset_password_token():
         return jsonify({"email": email, "reset_token": token})
 
 
-@app.route('/update_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """ Update password end-point
     """
