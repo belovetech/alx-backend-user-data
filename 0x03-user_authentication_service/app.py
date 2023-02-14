95d037fc9f0fb9d66cc68d786456d304c362c503
belovetech <belovetech@gmail.com>
2023-02-14 21:12:25 +0100

    [Feat]: update password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -119,9 +119,8 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
-
-    payload = {"email": email, "message": "Password updated"}
-    return jsonify(payload), 200
+    else:
+        return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
