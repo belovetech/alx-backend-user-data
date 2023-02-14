c215bb535b8b86fbdeeb647487cd35c376fa1efd
belovetech <belovetech@gmail.com>
2023-02-14 19:11:18 +0100

    [Feat]: logout user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic user flask application
 """
 from auth import Auth
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
@@ -48,6 +48,18 @@ def login():
             abort(401)
     except NoResultFound:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Logout user
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(session_id)
+        redirect('/', code=302)
+    abort(403)
 
 
 if __name__ == "__main__":
