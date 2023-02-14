8334a4957fff6588b6f5c7b2b528765536d196f9
belovetech <belovetech@gmail.com>
2023-02-14 18:41:33 +0100

    [Update]: login user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,9 @@
 """
 Basic user flask application
 """
-from flask import Flask, jsonify, request
 from auth import Auth
-
+from flask import Flask, jsonify, request, abort
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -29,6 +29,25 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError as err:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """Login user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.valid_login(email, password)
+    except NoResultFound:
+        abort(401)
+    else:
+        if user:
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
 
 
 if __name__ == "__main__":
