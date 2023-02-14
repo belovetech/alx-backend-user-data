8f45b073f76d386107e7c43087ed862609c946f5
belovetech <belovetech@gmail.com>
2023-02-14 19:03:21 +0100

    [Feat]: destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,10 @@ class Auth:
             except NoResultFound:
                 return None
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy session
+        """
+        if user_id:
+            self._db.update_user(user_id, session_id=None)
+        return None
