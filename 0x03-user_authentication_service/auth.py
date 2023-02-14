96107abd650fa0bdb682edd3d90a4c2cd712dc65
belovetech <belovetech@gmail.com>
2023-02-14 20:22:38 +0100

    [Feat]: get reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,15 @@ class Auth:
         if user_id:
             self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Get reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            token = str(uuid.uuid4())
+            self._db.update_user(user.id, reset_token=token)
+            return token
