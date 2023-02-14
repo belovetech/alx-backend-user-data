10e8697cd308815748d1e33324717f68df1d74da
belovetech <belovetech@gmail.com>
2023-02-14 19:36:54 +0100

    [Update]: logout user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,7 +65,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
 
     if user:
-        AUTH.destroy_session(session_id)
+        AUTH.destroy_session(user.id)
         return redirect('/')
     else:
         abort(403)
