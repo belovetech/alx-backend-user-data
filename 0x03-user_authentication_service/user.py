9fdbce24858728b632b9dac0d45fbd1c09280e4c
belovetech <belovetech@gmail.com>
2023-02-14 11:23:44 +0100

    [Update]: change varchar length

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(255), nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=False)
-    reset_token = Column(String(255), nullable=False)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
