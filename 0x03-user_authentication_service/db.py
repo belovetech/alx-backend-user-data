56452181b463a0a77fd8b1187d5400050ff8d064
belovetech <belovetech@gmail.com>
2023-02-15 11:17:16 +0100

    [Fix]: checj same thread connection error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,10 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine(
+            "sqlite:///a.db",
+            echo=False,
+            connect_args={"check_same_thread": False})
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
